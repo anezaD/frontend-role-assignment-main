69aa0e1f3b9208c7def36e36244ebdcda30e84c4
Aneza <aneza.dok@gmail.com>
2024-05-09 14:32:21 +0300

    Marker movement and scale

--- src/components/canvas/Canvas.tsx
+++ src/components/canvas/Canvas.tsx
@@ -42,45 +42,61 @@ export default function Canvas({ map, changeState }: { map: Leaflet.Map, changeS
     }, [changeState]);
 
     useEffect(() => {
-        if (fabricCanvas) {
-            fabricCanvas.clear();
+        const getResolution = (center: any, zoom: number) => {
+            // Resolution in meters per pixel. 40075016 is the length of the equator in meters.
+            return (
+                (Math.cos((center.lat * Math.PI) / 180) * 40075016) /
+                (256 * Math.pow(2, zoom))
+            );
+        }
+        const updateMarkerPositions = () => {
+            if (fabricCanvas) {
+                fabricCanvas.clear();
+                // Get the current latitude and longitude
+                const currentLatlng = map.getCenter();
+                // Get the current zoom level
+                const currentZoomLevel = map.getZoom();
+                const resolution = getResolution(currentLatlng, currentZoomLevel);
 
-            PLAYERS_DATA.forEach(item => {
-                const latitude = item.position.latitude;
-                const longitude = item.position.longitude;
-                const team = item.teamName;
+                PLAYERS_DATA.forEach(item => {
+                    const latitude = item.position.latitude;
+                    const longitude = item.position.longitude;
+                    const team = item.teamName;
 
-                // Convert latitude and longitude to map coordinates
-                const latLng = new Leaflet.LatLng(latitude, longitude);
-                const point = map.latLngToContainerPoint(latLng);
+                    // Convert latitude and longitude to map coordinates
+                    const latLng = new Leaflet.LatLng(latitude, longitude);
+                    const point = map.latLngToContainerPoint(latLng);
 
-                const shape = team === 'red' ? new fabric.Circle({
-                    left: point.x,
-                    top: point.y,
-                    radius: 5,
-                    fill: 'blue',
-                    strokeWidth: 2,
-                    stroke: 'white',
+                    const shape = team === 'red' ? new fabric.Circle({
+                        left: point.x,
+                        top: point.y,
+                        radius: 5 / resolution,
+                        fill: 'blue',
+                        strokeWidth: 2 / resolution,
+                        stroke: 'white',
 
-                }) : new fabric.Rect({
-                    left: point.x,
-                    top: point.y,
-                    width: 10,
-                    height: 10,
-                    fill: 'white',
-                    strokeWidth: 2,
-                    stroke: 'red',
+                    }) : new fabric.Rect({
+                        left: point.x,
+                        top: point.y,
+                        width: 10 / resolution,
+                        height: 10 / resolution,
+                        fill: 'white',
+                        strokeWidth: 2 / resolution,
+                        stroke: 'red',
+                    });
+
+                    shape.on('selected', function () {
+                        dispatch(removePlayerAction(item.id));
+                        dispatch(showNotificationAction({ name: item.name, id: item.id, team: item.teamName }))
+                        fabricCanvas.remove(shape);
+                    });
+
+                    return fabricCanvas.add(shape);
                 });
-
-                shape.on('selected', function () {
-                    dispatch(removePlayerAction(item.id));
-                    dispatch(showNotificationAction({ name: item.name, id: item.id, team: item.teamName }))
-                    fabricCanvas.remove(shape);
-                });
-
-                return fabricCanvas.add(shape);
-            });
+            };
         };
+
+        map.on('moveend zoomend', updateMarkerPositions);
 
     }, [map, fabricCanvas, PLAYERS_DATA]);
 
